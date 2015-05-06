b9974e8738ad6c810775974652f94f9c64bd3939
spectre6000 <spectre6000@gmail.com>
2015-05-06 17:31:51 -0600

    Log in/Log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def log_in(user)
+    remember_token = user.generate_token
+    cookies.permanent.signed[:remember_token] = remember_token
+    user.update_attribute(:remember_token, remember_token)
+    current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(remember_token: cookies.signed[:remember_digest])
+  end
+
+  def log_out
+    @current_user = nil
+    cookies.signed[:remember_token] = nil
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,25 @@
 class User < ActiveRecord::Base
+  before_create {generate_token}
+  before_save {email.downcase!}
+
+  validates :name, presence: true, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :password_digest, presence: true
   has_secure_password
 
-  def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  def generate_token
+    self.remember_token = encrypted_token
   end
-  
+
+  private
+    
+    def token
+      SecureRandom.urlsafe_base64
+    end
+
+    def encrypted_token
+      Digest::SHA1.hexdigest(token.to_s)
+    end
+
 end
